016d86cc3e90b5b1ae0acb957d7c82abc4d54df4
berryhe <hebianyu@agora.io>
2021-10-18 14:00:35 +0800

    feat: server shutdown

--- week_01/main.go
+++ week_01/main.go
@@ -1,19 +1,49 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"fmt"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/berryhe/cloud-native-curriculum/week_01/transport"
 )
 
 func main() {
 	transport.Version = os.Getenv("VERSION")
-
-	http.HandleFunc("/", transport.HandleRootPath)
-	http.HandleFunc("/healthz", transport.HandleHealthz)
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		panic(err)
+	mux := http.DefaultServeMux
+	mux.HandleFunc("/", transport.HandleRootPath)
+	mux.HandleFunc("/healthz", transport.HandleHealthz)
+	srv := http.Server{
+		Addr:    ":8080",
+		Handler: mux,
 	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			if !errors.Is(err, http.ErrServerClosed) {
+				panic(err)
+			}
+		}
+	}()
+
+	fmt.Println("server start.....")
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	<-ctx.Done()
+
+	stop()
+
+	fmt.Println("server shutting down gracefully")
+
+	// 给程序最多 5 秒时间处理正在服务的请求
+	timeoutCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(timeoutCtx); err != nil {
+		fmt.Println(err)
+	}
+
 }
